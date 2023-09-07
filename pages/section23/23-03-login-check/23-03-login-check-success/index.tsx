1d5a2685067039ed1db8eadfae64f63994558f57
Sun Choi <sunyfun1125@gmail.com>
2023-09-07 10:19:34 +0200

    hoc

--- pages/section23/23-03-login-check/23-03-login-check-success/index.tsx
+++ pages/section23/23-03-login-check/23-03-login-check-success/index.tsx
@@ -1,7 +1,7 @@
 import { gql, useQuery } from "@apollo/client";
-import type { Query } from "../../../src/commons/types/generated/types";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { Query } from "../../../../src/commons/types/generated/types";
 
 const FETCH_USER_LOGGED_IN = gql`
   query {
@@ -20,7 +20,7 @@ export default function LocalStorageSuccessPage(): JSX.Element {
   // 로컬스토리지에 accessToken이 없으면 로그인은 해달라고 하고 로그인페이지로 다시 이동
   useEffect(() => {
     if (localStorage.getItem("accessToken") === null) {
-      alert("로그인을 먼저 하세요!"); 
+      alert("로그인을 먼저 하세요!");
       void router.push("/section23/23-03-login-check");
     }
   }, []);
