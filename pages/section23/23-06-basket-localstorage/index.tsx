db41a59f759f8d174c958a8f8eaa78810d0b52cd
Sun Choi <sunyfun1125@gmail.com>
2023-09-08 21:09:53 +0200

    basket

--- pages/section23/23-06-basket-localstorage/index.tsx
+++ pages/section23/23-06-basket-localstorage/index.tsx
@@ -1,9 +1,7 @@
-// 여기서부터 플레이그라운드 서버 주소를 변경헸다. 코드젠도 변경
-// 따라서 이전의 타입스크립트에 문제가 생긴것들은 무시
-
 import { useQuery, gql } from "@apollo/client";
 import styled from "@emotion/styled";
 import {
+  Board,
   Query,
   QueryFetchBoardsArgs,
 } from "../../../src/commons/types/generated/types";
@@ -27,16 +25,17 @@ const Column = styled.div`
   width: 25%;
 `;
 
-const Page = styled.span`
-  cursor: pointer;
-  font-size: 18px;
-`;
+
 
 export default function PagiNationPage() {
   const { data } = useQuery<Pick<Query, "fetchBoards">, QueryFetchBoardsArgs>(
     FETCH_BOARDS
   );
-  const onClickBasket = () => {};
+  const onClickBasket = (basket: Board) => () => {
+    console.log(basket);
+    const baskets = [basket];
+    localStorage.setItem("baskets", baskets);
+  };
 
   return (
     <>
@@ -44,7 +43,7 @@ export default function PagiNationPage() {
         <Row key={el._id}>
           <Column>{el._id}</Column>
           <Column>{el.writer}</Column>
-          <button onClick={onClickBasket} id={el.id}> 장바구니 담기</button>
+          <button onClick={onClickBasket(el)}> 장바구니 담기</button>
         </Row>
       ))}
     </>
