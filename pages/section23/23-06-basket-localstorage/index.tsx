235ba10dace17a644d6475addc462635b3c4ec89
Sun Choi <sunyfun1125@gmail.com>
2023-09-11 09:29:41 +0200

    basket

--- pages/section23/23-06-basket-localstorage/index.tsx
+++ pages/section23/23-06-basket-localstorage/index.tsx
@@ -38,7 +38,7 @@ export default function PagiNationPage() {
     // - 로컬스토리지에 문자열 형태로 저장되어있는것을 다시 자바스크립형태로 바꿔줘야함
     // - basket안에 뭐가 있으면 그걸 JSON.parse하겠지만 아무것도 없으면 그건 변형이 안되고 그냥 null이기 떄문에
     //   null일 경우엔 "[]"를 넣어주라는 것
-    const baskets = JSON.parse(localStorage.getItem("basket") ?? "[]");
+    const baskets: Board[] = JSON.parse(localStorage.getItem("basket") ?? "[]");
 
     // 2. 추가할 것 장바구니에 담기
     baskets.push(basket);
