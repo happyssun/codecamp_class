0de3b800b15fa8c7ff246d25e8e80092820b7b42
Sun Choi <sunyfun1125@gmail.com>
2023-10-07 19:10:44 +0200

    folder

--- pages/section10/10-01-typescript-boards/new/index.tsx
+++ pages/section10/10-01-typescript-boards/new/index.tsx
@@ -1,4 +1,4 @@
-import BoardWrite from "../../../src/components/units/board/10-write/BoardWrite.container";
+import BoardWrite from "../../../../src/components/units/board/10-write/BoardWrite.container";
 
 export default function BoardWritePage() {
   //리턴안의 내용은 이것과 같다 {BoardWrite({isEdit:false})}
