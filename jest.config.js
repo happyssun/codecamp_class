80369ad666e6b5125d5ec629c126eccd5f122a22
Sun Choi <sunyfun1125@gmail.com>
2024-02-04 15:55:03 +0100

    jest

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,14 @@
 const nextJest = require("next/jest");
 
-/** @type {import('jest').Config} */
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
 // Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
 const config = {
-  coverageProvider: "v8",
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleDirectories: ["node_modules", "<rootDir>/"],
